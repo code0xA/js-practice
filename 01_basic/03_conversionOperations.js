680863a56226f5ae5f209ded58d7ddd2ea1e7d20
Anam <programming.anam@gmail.com>
2025-04-22 11:25:48 +0530

    conversions

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -1,9 +1,9 @@
 let score = "33abc"
-console.log(typeof(score));
+//console.log(typeof(score));
 
 let numScore = Number(score)
-console.log(numScore);
-console.log(typeof(numScore));
+//console.log(numScore);
+//console.log(typeof(numScore));
 
 //"33" => 33
 //"33abc" => NaN  -- It stand for Not a Number, however, its type is number i.e. typeof(NaN)=number
@@ -11,7 +11,7 @@ console.log(typeof(numScore));
 
 let isLoggedIn = undefined;
 let inBoolean = Boolean(isLoggedIn)
-console.log(inBoolean);
+//console.log(inBoolean);
 
 //"" => false
 //non-empty string will give true
@@ -20,6 +20,30 @@ console.log(inBoolean);
 
 let digit = 33
 let inString = String(digit)
-console.log(typeof(inString))
-console.log(inString);
+//console.log(typeof(inString))
+//console.log(inString);
+
+//OPERATIONS
+//+, -, / , % , *, **
+
+let str1 = "hello"
+let str2 = " anam"
+console.log(str1+ str2) //string concatenation
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + "2"); //12
+console.log("1" + 2 + 2); //122 --> all treated as string
+console.log(1 + 2 + "2"); //32 --> first few added until string concatenation
+// converts to string when required.
+
+let num = 100
+console.log(num++);
+console.log(++num);
+
+
+
+
+
+
 
