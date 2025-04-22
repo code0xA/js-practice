ac458c8d8107ebb4505d5a32294f0a16341f381d
Anam <programming.anam@gmail.com>
2025-04-22 08:59:39 +0530

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treats all JS code as newer version
+
+//alert(3+6)  //we are using node js, not browser
+ 
+let name = "anam"
+let age = 17
+let isLoggedIn = false;
+let state;
+
+//number => 2 to the power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefines => 
+//symbol => for uniqueness
+
+console.log(typeof "anam"); //returns 'string'
+console.log(typeof age); //returns 'number'
+console.log(typeof null); //returns 'object'. null is an object
+console.log(typeof undefined); //returns 'undefined'. Is its own type of datatype/
+
+
