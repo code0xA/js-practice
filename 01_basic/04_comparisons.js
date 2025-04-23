f69b0b099fd0ec8e52bf0aad5b3bd86f457e2b90
Anam <programming.anam@gmail.com>
2025-04-23 11:36:07 +0530

    Comparisons

---
+++ 01_basic/04_comparisons.js
@@ -0,0 +1,18 @@
+//comparison operators
+//>, >=, <, <=, ==
+
+// console.log("2">1)
+// console.log("02">1); 
+//both true
+
+console.log(null==0); //false
+console.log(null>0); //false
+console.log(null>=0); //true
+//equality chech(==) and comparisons(>, <, >=, <=) work differently
+//comparions convert null to number, treating it as 0.
+
+//=== is strict check
+console.log("2" == 2); //true
+console.log("2" === 2); //false
+
+
