2900a12997db87e776943bffa7b64c4f239d1d20
Anam <programming.anam@gmail.com>
2025-04-24 23:02:50 +0530

    string add

---
+++ 01_basic/05_string.js
@@ -0,0 +1,17 @@
+const name = "anam"
+const repoCount = 20
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('anam-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('m'));
